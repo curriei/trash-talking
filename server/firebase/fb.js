9c519e807767cf2e9f627356a3a329d2fb618af0
iancurrie <curriei@mcmaster.ca>
2021-01-17 17:34:21 -0500

    Permanent firebase authentication updates.

--- server/firebase/fb.js
+++ server/firebase/fb.js
@@ -1,10 +1,8 @@
 const admin = require('firebase-admin/lib/index');
 
 //IAM Service Account Key Json (not added to git)
-const service_account = require(`./trashtalking-96ed8-88707e3f8626.json`);
-
 admin.initializeApp({
-    credential: admin.credential.cert(service_account)
+    credential: admin.credential.applicationDefault()
 });
 
 const db = admin.firestore();
