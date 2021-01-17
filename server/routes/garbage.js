63f9a9c8ad669c2513977841aa9f88a0df8a04ac
iancurrie <curriei@mcmaster.ca>
2021-01-17 02:40:36 -0500

    more temp

--- server/routes/garbage.js
+++ server/routes/garbage.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const auth = require('../controllers/auth.js');
 const garbageControllers = require('../controllers/garbage.js');
+const binControllers = require('../controllers/bins.js');
 
 router.get('/', auth.verifyToken, garbageControllers.garbageQuery);
-router.get('/current', auth.verifyToken, garbageControllers.current);
+router.get('/current', auth.verifyToken,binControllers.current);
 
 module.exports = router;
