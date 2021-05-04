c8ec043b80011710f63702f10fd04509c61de162
Alexie McDonald <mcdona16@mcmaster.ca>
2021-05-04 16:57:13 -0400

    Update environment.prod.ts

--- frontend/TrashTalkingWeb/src/environments/environment.prod.ts
+++ frontend/TrashTalkingWeb/src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
   firebaseConfig: {
-    apiKey: "AIzaSyCEwjRn9Irpv7ffndsEKGGZwIlFLX0uu1Q",
-    authDomain: "trashtalking-96ed8.firebaseapp.com",
-    databaseURL: "https://trashtalking-96ed8.firebaseio.com",
-    projectId: "trashtalking-96ed8",
-    storageBucket: "trashtalking-96ed8.appspot.com",
-    messagingSenderId: "137442265925",
-    appId: "1:137442265925:web:3c273d45373132e49e0c2c"
+    apiKey: "xxx",
+    authDomain: "xxx",
+    databaseURL: "xxx",
+    projectId: "xxx",
+    storageBucket: "xxx",
+    messagingSenderId: "xxx",
+    appId: "xxx"
   }
 };
